cfe4c158a9f60206d7344ecb0a1256ae7afef25e
Serg <freekey@list.ru>
2023-10-26 23:47:28 +0400

    bags clear

--- src/main/java/ru/sergjavacode/Main.java
+++ src/main/java/ru/sergjavacode/Main.java
@@ -10,9 +10,7 @@ public class Main {
     static BlockingQueue<String> symbolAQueue = new ArrayBlockingQueue<>(100);
     static BlockingQueue<String> symbolBQueue = new ArrayBlockingQueue<>(100);
     static BlockingQueue<String> symbolCQueue = new ArrayBlockingQueue<>(100);
-    static String maxStringA = new String();
-    static String maxStringB = new String();
-    static String maxStringC = new String();
+
     static int maxCountA;
     static int maxCountB;
     static int maxCountC;
@@ -39,125 +37,25 @@ public class Main {
                     throw new RuntimeException(e);
                 }
 
-                System.out.println("\n" + i);
+                System.out.println(i);
 
             }
             System.out.println();
         });
 
+
         Thread readThreadA = new Thread(() -> {
-            if (writeThread.isAlive()) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            ;
-            AtomicInteger count = new AtomicInteger(0);
-            AtomicInteger max = new AtomicInteger(0);
-            String stringA;
-            while (!symbolAQueue.isEmpty()) {
-                System.out.println("СчитаемA");
-                try {
-                    stringA = symbolAQueue.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                String finalStringA = stringA;
-                IntStream.range(0, stringA.length())
-                        .forEach(x -> {
-                            if (finalStringA.charAt(x) == 'a') {
-                                count.addAndGet(1);
-                            }
-                        });
-                if (count.get() > max.get()) {
-                    max.set(count.get());
-                    maxStringA = stringA;
-                }
-                count.set(0);
-
-            }
-            maxCountA = max.get();
-            max.set(0);
-
+            maxCountA = readMax(symbolAQueue);
         });
 
         Thread readThreadB = new Thread(() -> {
-            if (writeThread.isAlive()) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            ;
-            AtomicInteger count = new AtomicInteger(0);
-            AtomicInteger max = new AtomicInteger(0);
-            String stringB;
-            while (!symbolBQueue.isEmpty()) {
-                System.out.println("СчитаемB");
-                try {
-                    stringB = symbolBQueue.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                String finalStringB = stringB;
-                IntStream.range(0, stringB.length())
-                        .forEach(x -> {
-                            if (finalStringB.charAt(x) == 'b') {
-                                count.addAndGet(1);
-                            }
-                        });
-                if (count.get() > max.get()) {
-                    max.set(count.get());
-                    maxStringB = stringB;
-                }
-                count.set(0);
-            }
-            maxCountB = max.get();
-            max.set(0);
+            maxCountB = readMax(symbolBQueue);
         });
 
         Thread readThreadC = new Thread(() -> {
-            if (writeThread.isAlive()) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            ;
-            AtomicInteger count = new AtomicInteger(0);
-            AtomicInteger max = new AtomicInteger(0);
-            String stringC;
-            while (!symbolCQueue.isEmpty()) {
-                System.out.println("СчитаемC");
-                try {
-                    stringC = symbolCQueue.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                String finalStringC = stringC;
-                IntStream.range(0, stringC.length())
-                        .forEach(x -> {
-                            if (finalStringC.charAt(x) == 'c') {
-                                count.addAndGet(1);
-                            }
-                        });
-                if (count.get() > max.get()) {
-                    max.set(count.get());
-                    maxStringC = stringC;
-                }
-                count.set(0);
-            }
-            maxCountC = max.get();
-            max.set(0);
+            maxCountC = readMax(symbolCQueue);
         });
         writeThread.start();
-        // Thread.sleep(10);
         readThreadA.start();
         readThreadB.start();
         readThreadC.start();
@@ -168,6 +66,41 @@ public class Main {
         System.out.println("maxStringA" + '\n' + maxCountA + '\n');
         System.out.println("maxStringB" + '\n' + maxCountB + '\n');
         System.out.println("maxStringC" + '\n' + maxCountC + '\n');
+
+    }
+
+    static int readMax(BlockingQueue<String> symbolQueue) {
+//        if (writeThread.isAlive()) {
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicInteger max = new AtomicInteger(0);
+        String str;
+        while (!symbolQueue.isEmpty()) {
+            System.out.println("Считаем");
+            try {
+                str = symbolQueue.take();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            String finalstr = str;
+            IntStream.range(0, str.length())
+                    .forEach(x -> {
+                        if (finalstr.charAt(x) == 'c') {
+                            count.addAndGet(1);
+                        }
+                    });
+            if (count.get() > max.get()) {
+                max.set(count.get());
+            }
+            count.set(0);
+        }
+        return max.get();
     }
 
     public static String generateText(String letters, int length) {
